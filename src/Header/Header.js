61e57f3f726e8b2366ff856c1a393cac17955225
Kanya <sKCuA2@kua.local>
2022-02-15 10:38:13 -0800

    added spaces between the header links

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -9,10 +9,10 @@ function Header() {
             <h1>DepressGo</h1>
           </Link>
       <nav>
-          <Link to="/">Home</Link>
-          <Link to="/Main/QuoteOfDay">Get The Quote The Day</Link>
-          <Link to="/Main/StayInspired">Stay Inspired</Link>
-          <Link to="/Main/ContactUs">Contact Us</Link>
+          <Link to="/">Home </Link>
+          <Link to="/Main/QuoteOfDay"> The Quote The Day </Link>
+          <Link to="/Main/StayInspired">Stay Inspired </Link>
+          <Link to="/Main/ContactUs">Contact Us </Link>
           <Link to="/Main/NeedHelp">
             Need Help? Speak To A Licensed Therapist
           </Link>
