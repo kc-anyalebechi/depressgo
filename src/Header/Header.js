1e97afdae9e43455fb17bc1f5a215e5043a6d650
Kanya <sKCuA2@kua.local>
2022-02-15 11:19:22 -0800

    working on the header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import './header.css'
-
+import "./header.css";
 
 function Header() {
   return (
     <div>
-      <div className='menu-section'>
-      <Link to='/'>
+      <div className="menu-section">
+        <div className="logo">
+          <Link to="/">
             <h1>DepressGo</h1>
           </Link>
-      <nav>
+        </div>
+        <div className="nav">
           <Link to="/">Home </Link>
           <Link to="/Main/QuoteOfDay"> The Quote The Day </Link>
           <Link to="/Main/StayInspired">Stay Inspired </Link>
@@ -18,11 +19,16 @@ function Header() {
           <Link to="/Main/NeedHelp">
             Need Help? Speak To A Licensed Therapist
           </Link>
-        </nav>
         </div>
-
+        <div className="theme-quote">
+          <p>
+            “Today is the first day of the rest of your life.” ~ Attributed to
+            Charles E. “Chuck” Dederich, Sr
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
