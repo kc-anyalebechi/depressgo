f7cb35999c0e712995c227b2d41ecee235ee81ff
Kanya <sKCuA2@kua.local>
2022-02-16 20:50:49 -0800

    testing code to create the responsive navbar

---
+++ src/Navigation.js
@@ -0,0 +1,21 @@
+import { Navbar, Nav, Container } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar collapseOnSelect fixed="top" expand="sm" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar=nav" />
+          <Navbar.Collapse id="responsive-narbar-nav">
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/Main/QuoteOfDay">Quote of the Day</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation; 
